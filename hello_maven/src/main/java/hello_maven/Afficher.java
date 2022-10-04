b107059f61e5c432d0c250d2a4e2b6f465995b66
ELBARHMI <elbarhmi.oussama@gmail.com>
2022-10-04 11:01:26 +0200

    Afficher

---
+++ hello_maven/src/main/java/hello_maven/Afficher.java
@@ -0,0 +1,16 @@
+package hello_maven;
+
+public class Afficher {
+	
+	
+	/**
+	 * 
+	 * Afficher à l'écran hello Maven
+	 */
+	
+	public static void main(String[] args) {
+		System.out.println("Hello Paris");
+	}
+	
+	
+}
