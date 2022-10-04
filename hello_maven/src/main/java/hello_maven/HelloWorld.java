133d655b12a3b98eab3b727f6452fac7f164bc69
ELBARHMI <elbarhmi.oussama@gmail.com>
2022-10-04 10:14:24 +0200

    first commit

---
+++ hello_maven/src/main/java/hello_maven/HelloWorld.java
@@ -0,0 +1,14 @@
+package hello_maven;
+
+public class HelloWorld {
+	
+	/**
+	 * 
+	 * Afficher à l'écran hello Maven
+	 */
+	
+	public static void main(String[] args) {
+		System.out.println("Hello Maven");
+	}
+
+}
